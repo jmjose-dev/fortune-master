42305958b560ec7f306bb4dd4437d137ffaaff01
Jan Michael Jose <janmichael.jose@gmail.com>
2020-07-19 22:43:23 +0800

    added test fortune according to fortunes.md

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ var methodOverride = require("method-override");
 var sanitizer = require("express-sanitizer");
 var port = process.env.PORT || 3000;
 const request = require('request');
+const url = 'mongodb://127.0.0.1:27017/fortunesdb';
+//connect defined db to mongoose
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(url, {useNewUrlParser: true});
+var fortunesSchema = new mongoose.Schema({
+    id: String,
+    fortune: String
+});
+var fortunesList = mongoose.model("allfortunes",fortunesSchema);
+fortunesList.insertMany({fort:"A friend's frown is better than a fool's smile."});
+
+
 
 app.use(bP.urlencoded({extended: true}));
 app.use(ex.static("public"));

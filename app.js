072fb4f2046bc015956941e3d4949a6f9ed5b195
Jan Michael Jose <janmichael.jose@gmail.com>
2020-07-20 10:07:00 +0800

    Random gen of fortunes according to mongo file

--- app.js
+++ app.js
@@ -8,25 +8,55 @@ var sanitizer = require("express-sanitizer");
 var port = process.env.PORT || 3000;
 const request = require('request');
 const url = 'mongodb://127.0.0.1:27017/fortunesdb';
+const lineReader = require('line-reader');
+var firstTime;
+var forts = [];
+var x=0;
+var fortuneTotal;
+var randValue;
 //connect defined db to mongoose
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect(url, {useNewUrlParser: true});
 var fortunesSchema = new mongoose.Schema({
     id: String,
-    fortune: String
+    fort: String
 });
 var fortunesList = mongoose.model("allfortunes",fortunesSchema);
-fortunesList.insertMany({fort:"A friend's frown is better than a fool's smile."});
-
-
-
 app.use(bP.urlencoded({extended: true}));
 app.use(ex.static("public"));
 app.use(methodOverride("_method"));
 app.use(sanitizer());
 app.get("/", function (req, res) {
-    res.render("index.ejs");
+    if (firstTime==1)
+    {
+      res.render("index.ejs",{firstTime:firstTime});
+    }
+    else
+    {
+      res.render("index.ejs",{randFortune: forts[randValue], firstTime:firstTime});
+    }
+});
+app.get("/getfortune", function (req, res){
+  fortunesList.find({}, function(err, flist){
+    if(err){
+      console.log(err);
+    }
+    else
+    {
+      firstTime = 0;
+      flist.forEach(function(flist){
+        forts[x]=flist.fort;
+        x++;
+      });
+      fortuneTotal = forts.length;
+      randValue = Math.floor(Math.random() * fortuneTotal);
+      console.log(forts[randValue]);
+      res.redirect("/");
+    }
+  });
+
 });
 app.listen(port, function () {
+    
     console.log("Server is running");
   });
